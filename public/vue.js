aeafd4bb1fda01767415be2bdea8803c804b68b8
unknown <lhestermoon3011@gmail.com>
2018-10-24 17:20:17 +0800

    Adding methods to a form

--- public/vue.js
+++ public/vue.js
@@ -18,6 +18,45 @@
         .catch(err => {
           console.log(err);
         });
+    },
+    methods: {
+      addNote: function() {
+        let self = this;
+        let payload = {
+          title: self.title,
+          content: self.content,
+          author: self.author
+        };
+        axios
+          .post("/api/notes", payload)
+          .then(res => {
+            self.notes = res.data;
+            // self.notes.push({
+            //   id: 99,
+            //   title: self.title,
+            //   content: self.content,
+            //   author: self.author
+            // });
+          })
+          .catch(err => {
+            console.log(err);
+          });
+      },
+      clear: function() {
+        this.title = null;
+        this.content = null;
+        this.author = null;
+      },
+      delete: function(note) {
+        axios
+          .delete("/api/notes" + note.id)
+          .then(res => {
+            self.notes = res.data;
+          })
+          .catch(err => {
+            console.log(err);
+          });
+      }
     }
   });
   console.log(vue);
